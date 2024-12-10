3be7aaf1fac890c6377f24fb0c2488ff18a06e45
MayconDalsico <135442648+MayconDalsico@users.noreply.github.com>
2024-12-10 11:11:01 -0400

    mudando para localStorage
    a

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -168,8 +168,8 @@ class UserController {
   getUsersStorage() {
     let users = [];
 
-    if (sessionStorage.getItem("users")) {
-      users = JSON.parse(sessionStorage.getItem("users"));
+    if (localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"));
     }
 
     return users;
@@ -192,7 +192,7 @@ class UserController {
 
     users.push(data);
 
-    sessionStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("users", JSON.stringify(users));
   }
 
   addLine(dataUser) {
